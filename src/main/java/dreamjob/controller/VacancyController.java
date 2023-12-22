4df6a108207a3c7fcf1e42104c9474700bbfe278
Rustam Kadimov <kadim_r@mail.ru>
2023-12-22 17:24:47 +0500

    Создание вакансии

--- src/main/java/dreamjob/controller/VacancyController.java
+++ src/main/java/dreamjob/controller/VacancyController.java
@@ -1,12 +1,17 @@
 package dreamjob.controller;
 
 
+import dreamjob.model.Vacancy;
 import dreamjob.repository.MemoryVacancyRepository;
 import dreamjob.repository.VacancyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -22,5 +27,12 @@ public class VacancyController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancy.setCreationDate(LocalDateTime.now());
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
--- src/main/java/dreamjob/model/Vacancy.java
+++ src/main/java/dreamjob/model/Vacancy.java
@@ -12,6 +12,9 @@ public class Vacancy {
 
     private LocalDateTime creationDate;
 
+    public Vacancy() {
+    }
+
     public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
         this.title = title;
